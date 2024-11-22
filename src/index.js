e69e0241a08b6b5a5c3a9a33ecf30a7131b099ea
thiago <thiagoalexandredornelas@gmail.com>
2024-11-22 09:16:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function ExibirCtt(props){
-  return <p>Nome {props.nome} {props.sobrenome}, CPF {props.cpf}, Contato {props.contato}, Email {props.email} </p>
+  return <p>Nome {props.nome} {props.sobrenome}, cpf {props.cpf}, Contato {props.contato}, Email {props.email} </p>
 }
 
 function App(){
   return(
     <div>
-      <h1>Questão 1 - ExibirCtt</h1>
+      <h1>Questão 1</h1>
       <ExibirCtt nome = "João" sobrenome = "Araújo" cpf = "000.000.000-00" contato = "(84) 99204-1473" email = "jp@gmail.com"/>
       <ExibirCtt nome = "Thiago" sobrenome = "Alexandre" cpf = "111.111.111-11" contato = "(84) 99234-1473" email = "thiago@gmail.com"/>
       <ExibirCtt nome = "Jorge" sobrenome = "Matheus" cpf = "252.345.482-00" contato = "(84) 99202-2423" email = "jorge@gmail.com"/>
