792223071a75850a9a446f4cf57975dfb7cf3650
thiago <thiagoalexandredornelas@gmail.com>
2024-11-22 09:14:07 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ function App(){
   return(
     <div>
       <h1>Questão 1 - ExibirCtt</h1>
-      <ExibirCtt nome = "João" sobrenome = "Araújo" cpf = "000.000.000-00" contato = "(84) 99204-1473" email = "jpdr3551@gmail.com"/>
-      <ExibirCtt nome = "Thiago" sobrenome = "Alexandre" cpf = "111.111.111-11" contato = "(84) 99234-1473" email = "JeováJr@gmail.com"/>
-      <ExibirCtt nome = "Jorge" sobrenome = "Matheus" cpf = "222.222.222-22" contato = "(84) 99202-2423" email = "neymar@gmail.com"/>
-      <ExibirCtt nome = "Jaaziel" sobrenome = "Pereira" cpf = "333.333.333-33" contato = "(84) 93254-6463" email = "Brigadeiricesdaju@gmail.com"/>
-      <ExibirCtt nome = "Maysa" sobrenome = "Almeida" cpf = "444.444.444-44" contato = "(84) 99224-1365" email = "maysinha2015@gmail.com"/>
+      <ExibirCtt nome = "João" sobrenome = "Araújo" cpf = "000.000.000-00" contato = "(84) 99204-1473" email = "jp@gmail.com"/>
+      <ExibirCtt nome = "Thiago" sobrenome = "Alexandre" cpf = "111.111.111-11" contato = "(84) 99234-1473" email = "thiago@gmail.com"/>
+      <ExibirCtt nome = "Jorge" sobrenome = "Matheus" cpf = "252.345.482-00" contato = "(84) 99202-2423" email = "jorge@gmail.com"/>
     </div>
   )
 }
